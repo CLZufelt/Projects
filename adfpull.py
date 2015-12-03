e8b5d658359b8a6666e65b07cf63f791de916a1a
Charlie <CLZufelt@gmail.com>
2015-12-02 18:17:44 -0800

    Changes to adfpull.py

--- adfpull.py
+++ adfpull.py
@@ -11,7 +11,7 @@ import platform
 import subprocess
 import sys
 import time
-import zipfile
+import tarfile
 
 
 country = "US" #raw_input("Country (Ex. US): ")
@@ -26,8 +26,11 @@ fileName += "_" + city
 fileName += "_" + location
 fileName += "_" + today
 
-dev_list = [line.split("\t") for line in os.popen('adb devices').read().split("\n") if len(line.split("\t")) == 2]
-devices = [device[0] for device in dev_list]
+dev_list =
+devices = [device[0]
+           for device in [line.split("\t")
+            for line in os.popen('adb devices').read().split("\n")
+            if len(line.split("\t")) == 2]]
 
 
 def get_adf_list(devices):
@@ -60,7 +63,7 @@ def mkdir(fileName):
 def adf_pull(devices):
   for device in devices:
     raw_data, adf = get_adf_list(device)
-    start = time.time()
+    begin = time.time()
     for data in raw_data:
       time.sleep(5)
       start = time.time()
@@ -68,7 +71,64 @@ def adf_pull(devices):
       os.system("adb -s %s pull %s %s" % (device, pull_from, mkdir(fileName)))
       print "The pull took: "
       countup(start)
-    countup(start)
+    countup(begin)
+
+def create_json():
+  with open("properties.json", 'w') as properties:
+    properties_file = "{\n\t\"collection_timestamp\" : \"%s %s\",\n"\
+    % (str(datetime.date.today()), str(raw_input("Time of collect: "
+    "(hh:mm:ss)\n")))
+    properties_file += "\ntags\n\" : [\"collection_method_%s\", " \
+    % raw_input("Collection method:\nhandheld\nheadset\n")
+    properties_file += "\"lighting_%s\", " % raw_input("Lighting type:\n"
+    "varied\nstore\ndim\noutdoors_sunny\noutdoors_overcast"
+    "\nhouse_dim\nhouse_bright\n")
+    properties_file += "\"tilt_%s\", " % raw_input("Tilt:\n"
+    "vertical\ntilted\nhorizontal\nvaried\n")
+    properties_file += "\"layout_%s\", " % raw_input("Layout type:\n"
+    "portrait\nlandscape\nportrait_and_landscape\n")
+    properties_file += "device_yellowstone, "
+    properties_file += "\"orientation_%s\"]\n}\"" % raw_input("Orientation:\n"
+    "uniform\nvaried\n")
+    properties.write(properties_file)
+  with open("adf_properties.json", 'w') as adf_properties:
+    adf_json = "{\n\t\"tags\" : [\"%s\", " % \
+    raw_input("Algorithm scale (select all that apply):\n"
+    "\"algorithm_matching_small_scale\"\n\"algorithm_matching_medium_scale\"\n"
+    "\"algorithm_matching_large_scale\"\n\"algorithm_retrieval_small_scale\"\n"
+    "\"algorithm_retrieval_medium_scale\"\n"
+    "\"algorithm_retrieval_large_scale\"\n")
+    adf_json += "\"construction_%s\", " % raw_input("Construction type:\n"
+    "pose_graph\nVIWLS\npose_graph_with_structure\n")
+    adf_json += "\"floor_%s\", " % raw_input("Floors:\nmultiple\nsingle\n")
+    adf_json += "\"compression_%s\", " % raw_input("Compression type:\n"
+    "none\npq\n")
+    adf_json += "\"%s\", " % raw_input("Summarization:\n"
+    "summarization_none\nkeyframe_pruning_every_2nd\n"
+    "summarization_observation_count\n")
+    adf_json += "\"area_%s\", " % raw_input("Area:\nindoors\noutdoors\n"
+    "indoors_and_outdoors\n")
+    adf_json += "\"collection_method_%s\", " % raw_input("Collection method:"
+    "\nhandheld\nheadset\n")
+    adf_json += "\"lighting_%s\", " % raw_input("Lighting type:\n"
+    "varied\nstore\ndim\noutdoors_sunny\noutdoors_overcast"
+    "\nhouse_natural_dim\nhouse_natural_overcast"
+    "\nhouse_dim\nhouse_bright\n")
+    adf_json += "\"tilt_%s\", " % raw_input("Tilt:\n"
+    "vertical\ntilted\nhorizontal\nvaried\n")
+    adf_json += "\"layout_%s\", " % raw_input("Layout type:\n"
+    "portrait\nlandscape\nportrait_and_landscape\n")
+    adf_json += "\"device_%s\"]\n}" % raw_input("Device:\nyellowstone\n")
+    adf_properties.write(adf_json)
+
+def compress_files(destination, source_dir):
+  with tarfile.open(destination, "w:gz") as tar:
+    tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+def upload():
+  pass
+
+
 
 
 def countup(start_time):
@@ -121,4 +181,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
