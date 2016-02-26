9cff277fcfbd992e01838379723d0b5e823a38b4
Charlie <CLZufelt@gmail.com>
2016-02-26 09:11:14 -0800

    delete_data.py

--- Tango/delete_data.py
+++ Tango/delete_data.py
@@ -3,9 +3,16 @@
 import os, sys
 import time
 
-os.system("adb root")
-time.sleep(1)
-os.system("adb shell 'rm -rf /data/data/com.projecttango.tango/files/datasets/*'")
-print "Deleted datasets"
-os.system("adb shell 'rm -rf /sdcard/tangomapscreator/*'")
-print "Deleted adf's"
+
+devices = [device[0]
+          for device in [line.split("\t")
+          for line in os.popen('adb devices').read().split("\n")
+          if len(line.split("\t")) == 2]]
+
+for dev in devices:
+  os.system("adb root")
+  time.sleep(1)
+  os.system("adb shell 'rm -rf /data/data/com.projecttango.tango/files/datasets/*'")
+  print "Deleted datasets"
+  os.system("adb shell 'rm -rf /sdcard/tangomapscreator/*'")
+  print "Deleted adf's"
